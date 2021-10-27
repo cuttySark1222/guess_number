e23f6eeabf894f2f1dc3f0ec074a8683bab18b0e
Larry Zhou <webgrowbig@gmail.com>
2021-10-27 15:22:27 -0400

    Update Python file - add guess count

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,15 @@
 import random
 
 answer = random.randint(1, 100)
+user_try = 0
 
 while True:
-	guess = input("请猜一个数字： ")
+	guess = input("请猜一个数字（1~100）： ")
 	guess = int(guess)
+	user_try += 1
 	if guess == answer:
 		print("恭喜你，你猜对了！")
+		print(f'你一共猜了{user_try}次!')
 		break
 	elif guess > answer:
 		print("不对，答案要比你猜的小！")
